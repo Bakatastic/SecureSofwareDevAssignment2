378261899f070b283198ee11035e92810ead43f2
XingChan <Brian.xing.yu@hotmail.com>
2018-02-24 20:05:58 -0500

    changes to login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+	<title>Login</title>
+	<style type="text/css">
+	</style>
+	</head>
+	<body>
+		<h3>Login</h3>
+		<form action="login.php" method="POST" >
+			<table>
+				<tr>
+					<td>Username: </td>
+					<td><input type="text" name="userInput" required /></td>
+				</tr>
+				<tr>
+					<td>Password: </td>
+					<td><input type="text" name="emailInput" required /></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="submit" /></td>
+					<td></td>
+				</tr>			
+			</table>
+		</form>
+	</body>
+</html>
