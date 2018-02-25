d4aec33ede9b130961ac69346ff1ab5cc9e5d3fe
XingChan <Brian.xing.yu@hotmail.com>
2018-02-25 13:20:59 -0500

    prototype testing for blogging stuff

---
+++ visitorBlog.php
@@ -0,0 +1,43 @@
+<!--
+The main blog page. Visitors to the blog should be able to view a summary of recent blog entries and click on a blog post for an expanded/detailed view of that specific post. Blog posts should indicate the author and show their avatar.
+-->
+<!-- 
+Reference on expanding/closing blog post using jquery
+http://jsfiddle.net/eK8X5/13261/
+-->
+<html>
+	<head>
+	<title>View Blogs</title>
+	<style type="text/css">
+		.container {
+			width: 75%;
+			height-min: 50px;
+			background: black;
+			margin: auto;
+			padding:10px;
+		}
+		.avatar {
+			width: 15%;
+			background: red;
+			float: left;
+		}
+		.HeaderText {
+			margin-left: 15%;
+			background: white;
+		}
+	</style>
+	</head>
+	<body>
+		<div class='container'>
+			<div class='avatar'>
+				<img src="pulpitrock.jpg" alt="Mountain View">
+			</div>
+			<div class='HeaderText'>
+				<h3>TRDFJABGS</h3>
+			</div>
+			<div class='textContent'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</body>
+</html>
