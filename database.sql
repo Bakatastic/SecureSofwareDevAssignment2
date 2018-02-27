783a2a54dd9533bf6255ededce3efd83e1ffbd66
Bakatastic <Bakatastic@hotmail.com>
2018-02-27 13:29:41 -0500

    Updated schema

--- database.sql
+++ database.sql
@@ -1,22 +1,19 @@
 DROP DATABASE a2;
 CREATE DATABASE a2;
-USE a2;
+/c a2;
 
-CREATE TABLE users (
-    username	VARCHAR(20)		NOT NULL Primary Key,
-    password	VARCHAR(40)		NOT NULL
-	avatar		
+REATE TABLE users (
+    username	VARCHAR(20)			NOT NULL Primary Key,
+    password	VARCHAR(40)			NOT NULL,
+	email		VARCHAR(100)		NOT NULL,
+    avatar		bytea,
+	bio			VARCHAR(100)
 );
 
 CREATE TABLE posts (
-	postID		INT(10)			NOT NULL Primary Key auto_increment,
-	postText 	VARCHAR(1000)	NOT NULL,
-	username 	VARCHAR(20)		NOT NULL,
-	FOREIGN KEY (username) REFERENCES users(username)
-);
+    postID		serial primary key,
+    postText	VARCHAR(1000)		NOT NULL,
+    username	VARCHAR(20)			NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
 
-CREATE TABLE avatars (
-	username	VARCHAR(30),
-	imgOID		BYTEA
-	FOREIGN KEY (username) REFERENCES users(username)
 );
