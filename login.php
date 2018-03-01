fd7337bdd5fad506eb6d32ae230c8504ff34fbc1
Bakatastic <bakatastic@hotmail.com>
2018-02-28 23:04:42 -0500

    Account lock, 5 failed attempts, and non approve account filter implemented

--- login.php
+++ login.php
@@ -20,9 +20,10 @@
 		<h1>Login</h1>
 		<?php
 			//Set variables to empty values
-			$usernameErr = $paswordErr = "";
+			$usernameErr = $paswordErr = $otherErr = "";
 			$username = $password= "";
 			$fail = 0;
+			$conn = pg_connect("host=localhost dbname=a2 user=postgres password=password");
 			
 			if($_SERVER["REQUEST_METHOD"] == "POST"){
 				//Username regex to sanitize input before comparing database
@@ -48,7 +49,7 @@
 					$password = sanitize($_POST["password"]);
 					//Check input
 					if(!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/", $password)){
-						$passwordErr = "Incorrect Password<br>";
+						$passwordErr = "Incorrect Password format. Use at least 8 characters, 1 letter and 1 number<br>";
 						$fail = 1;
 					}
 					//Encrypt
@@ -57,38 +58,58 @@
 				
 				//Add to database if regex check passes
 				if($fail == 0){
-					$conn = pg_connect("host=localhost dbname=a2 user=postgres password=password");
-									$result = pg_query($conn, "SELECT * FROM users");
+					$result = pg_query($conn, "SELECT * FROM users");
 					while($user = pg_fetch_row($result)){
-						//This assumes that the username is the first column in the table
+						//Matches credentials
 						if($username == $user[0] && $password == $user[1]){
-							//Login stuff
-							echo "Logged in!";
-							$_SESSION["username"] = $_POST["username"];
-							//Send to another page
-							if ($checkProfile == 1)
-							{							
-								$checkProfile = 0;
-								//destination if coming from Profile.php
-								header("Location: profile.php");
-								exit();
-							} else 
-							{
-								//checks if the change password flag is up. redirects to correct page
-								if ($_SESSION['Change'] == 1){
-									$_SESSION['Change'] = 2;
+							if($user[6] == 'f'){
+								$otherErr = "Account not approved";
+							}
+							else if($user[7] == 't'){
+								$otherErr = "Account locked";
+							}
+							else{
+								//Login stuff
+								$_SESSION["username"] = $_POST["username"];
+								//Send to another page
+								if ($checkProfile == 1)
+								{							
+									$checkProfile = 0;
+									//destination if coming from Profile.php
 									header("Location: profile.php");
 									exit();
-								} else {
-									//if everything is successful. neutral destination
-									header("Location: postManagement.php");
-									exit();
+								} else 
+								{
+									//checks if the change password flag is up. redirects to correct page
+									if ($_SESSION['Change'] == 1){
+										$_SESSION['Change'] = 2;
+										header("Location: profile.php");
+										exit();
+									} else {
+										//if everything is successful. neutral destination
+										header("Location: postManagement.php");
+										exit();
+									}
 								}
 							}
 						}
+						else{
+							$otherErr = "Incorrect username or password";
+							//Increment failed login attempts. Lock if 5 fails
+							//$result = pg_query($conn, "SELECT * FROM users");
+							if($user[8]<4){ //It says 4 here, but in practice, it's 5.
+								if($user[7] == 'f'){
+									$result = pg_query($conn, "UPDATE users SET failedattempts = failedattempts + 1 WHERE username = '$_POST[username]';");
+								}
+								else{
+									$otherErr = "Account locked";
+								}
+							}
+							else{
+								$result = pg_query($conn, "UPDATE users SET accountlock = TRUE, failedattempts = 0 WHERE username = '$_POST[username]';");
+							}
+						}
 					}
-					echo "Login failed!";
-					//Place failed to login message here somewhere
 				}
 			}
 			
@@ -127,6 +148,7 @@
 				</table>
 			</form>
 			<?php
+				echo $otherErr;
 				echo $usernameErr;
 				echo $passwordErr;
 			?>
