876fef43b96e8a45d793d1a06041d673b7065ddc
XingChan <Brian.xing.yu@hotmail.com>
2018-02-28 20:25:56 -0500

    added avatar to visitorBlog.php

--- userAdmin.php
+++ userAdmin.php
@@ -25,5 +25,20 @@
 			<li><a href="visitorBlog.php">Visitor Blog</a></li>
 			<li><a href="login.php">Logout</a></li>
 		</ul>
+		
+		<div>
+			<table>
+				<?php $conn = pg_connect("host=localhost dbname=a2 user=postgres password=password");
+					$result = pg_query($conn, "SELECT * FROM users;");
+					$row = pg_fetch_row($result);
+					
+					while ($row = pg_fetch_row($result))
+					{
+						echo "<tr><td><a href='userDetails.php?username=" . $row[0]. "'>" . $row[0] . "</a></td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td></tr>";
+					}
+				?>
+			</table>
+		</div>
+		
 	</body>
 </html>
